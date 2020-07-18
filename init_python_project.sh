90b9b744876fbf40dcdb5a2ad504f6cc7b870738
uigctaw <uigctaw@protonmail.com>
2020-07-18 18:51:39 +0100

    mvp

---
+++ init_python_project.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+project=$(basename $PWD)
+
+read -p 'Create project and test folders? [yes]' agreed
+if [[ ${agreed:-yes} == yes ]]; then
+    mkdir $project
+    touch $project/__init__.py
+    mkdir tests
+    mkdir tests/unit
+    mkdir tests/integration
+fi
+
+read -p 'Create Python virtual environment? [yes]' agreed
+if [[ ${agreed:-yes} == yes ]]; then 
+    read -p 'Please enter folder name [.venv]' venv_folder
+    venv_folder=${venv_folder:-.venv}
+    venv_full_path=$venv_folder/bin/python
+    read -p 'Please enter python version [python3.8]' python_name
+    eval ${python_name:-python3.8} -m venv $venv_folder
+
+    read -p 'Create symlink "py" to virtual env Python executable?' py_sl
+    if [[ -z $py_sl ]]; then
+        ln -s $venv_full_path py
+    fi
+fi
+
+read -p 'Initialize Poetry toml file? [yes]' agreed_poetry
+if [[ ${agreed:-yes} == yes_poetry ]]; then
+    poetry init
+
+    if [[ -z $venv_folder ]]; then
+        question="Change Poetry's virtual env to the one created before?"
+        question="$question [yes]"
+        read -p "$question" agreed 
+        if [[ ${agreed:-yes} == yes ]]; then
+            poetry env use $venv_full_path
+        fi
+    fi
+fi
+
+read -p 'Initialize git repository? [yes]' agreed
+if [[ ${agreed:-yes} == yes ]]; then
+    git init
+fi
+
+read -p 'Create .gitignore with some defaults? [yes]' agreed
+if [[ ${agreed:-yes} == yes ]]; then
+    cat > .gitignore << EOM
+.*
+!/.gitignore
+dist
+*.egg-info
+**/__pycache__
+EOM
+
+cat .gitignore
+fi
+
+if [[ $venv_full_path ]]; then
+    question='Create flake8, mypy and bandit exec scripts? [yes]'
+    read -p "$question" agreed
+    if [[ ${agreed:-yes} == yes ]]; then
+        cat > flake8.sh << EOM
+#!/bin/bash
+$venv_full_path -m flake8 $project
+$venv_full_path -m flake8 tests
+EOM
+        chmod u+x flake8.sh 
+
+        cat > mypy.sh << EOM
+#!/bin/bash
+$venv_full_path -m mypy --strict $project
+$venv_full_path -m mypy --strict tests
+EOM
+        chmod u+x mypy.sh 
+
+        cat > bandit.sh << EOM
+#!/bin/bash
+$venv_full_path -m bandit -rq $project
+$venv_full_path -m bandit -rq tests
+EOM
+        chmod u+x bandit.sh 
+
+        if [[ ${agreed_poetry:-yes} == yes ]]; then
+            poetry add flake8
+            poetry add mypy
+            poetry add bandit
+        fi
+    fi
+fi
